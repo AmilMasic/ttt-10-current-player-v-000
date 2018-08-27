07efcd8c53a2a7c36299e7b995767c3b5ed67305
Amil Masic <amil.masic@gmail.com>
2018-08-27 15:18:10 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,13 +8,3 @@
 #     counter += 1
 #   end
 # end
-def turn_count(board)
-  tokens = ["X", "O"]
-  counter = 0
-  while counter <= 9
-    tokens.each do |token|
-      board[counter]
-      counter += 1
-    end
-  end
-end
